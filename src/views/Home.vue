5d4cf9c537bcce0cd21c62d6d6f06a69fccef34f
mouhsineelachbi <mouhsineelachbi@gmail.com>
2020-12-22 23:23:33 +0100

    how we use computed values

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,24 @@
 <template>
   <div class="home">
       <h1> Home </h1>
-      <h2> Refs </h2>
-      <p> {{ meOne.name }} - {{ meOne.age }}</p>
-      <button @click="updateMeOne">Update One</button>
-      <h2>Reactive</h2>
-      <p> {{ meTwo.name }} - {{ meTwo.age }}</p>
-      <button @click="updateMeTwo">Update Two</button>
+      <h2>{{  age   }}</h2>
       
   </div>
 </template>
 
 <script>
-import { reactive, ref } from 'vue';
+import { computed, reactive, ref } from 'vue';
 
 
 export default {
   name: 'Home',
   setup () {
+    // use computed values
+    let age = computed(() => {
+      return Math.floor(Math.random()*100000);
+    })
 
-    let meOne = ref({ name: 'Mouhsine', age: 25 }); // work better for external composition functions cz thy're retain reactivity when exposed
-    let meTwo = reactive({ name: 'Mythose', age: 25 }); // reactive doesn't work with primitive value like reactive('mouhsine') or reactive(1)
-    
-    function updateMeOne () {
-      meOne.value.age = Math.floor(Math.random()*100);
-    }
-
-    function updateMeTwo () {
-      meTwo.age = Math.floor(Math.random()*100);
-    }    
-
-    return { meOne, updateMeOne, meTwo, updateMeTwo }
+    return {  age }
   }
 }
 </script>
