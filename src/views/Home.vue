7218b1f70f97d117d24dca3108e38d131faf5205
mouhsineelachbi <mouhsineelachbi@gmail.com>
2020-12-22 22:56:34 +0100

    using ref to make properties reactive like in the options api data() function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,8 @@
       Home
       <p ref="p">Hello my name is {{ name }} and I'm {{ age }} years old</p>
       <button @click="handleClick">Click me</button>
+      <input type="text" name="name" id="name" v-model="name">
+      <button @click="age++">Increment Age</button>
   </div>
 </template>
 
@@ -13,19 +15,18 @@ import { ref } from 'vue';
 export default {
   name: 'Home',
   setup () {
-    let name = 'Mouhsine'
-    let age = 25;
+    const name = ref('Mouhsine')
+    const age = ref(25);
 
     //  Creating a ref to the paragraphe in the template
-    let p = ref(null);
+    // let p = ref(null);
 
     function handleClick () {
-      console.log(p.value);
-      p.value.classList.add('test')
-      p.value.textContent = 'this is a lie'
+      name.value = 'Not Mouhsine';
+      age.value = 99;
     }
 
-    return {  name, age,  handleClick, p }
+    return {  name, age,  handleClick }
   }
 }
 </script>
