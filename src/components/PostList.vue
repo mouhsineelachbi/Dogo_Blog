cee14f3c155e09d0b99fa5b6b0da1f2e9ebbd143
mouhsineelachbi <mouhsineelachbi@gmail.com>
2020-12-24 20:42:51 +0100

    use props and substring for snippet post body

---
+++ src/components/PostList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="post-list">
+        <div v-for="post in posts" :key="post.id">
+            <SinglePost :post="post"/>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import SinglePost from './SinglePost.vue';
+
+export default {
+    props: ['posts'],
+    components: { SinglePost },
+    // if we want to use props in the setup we do what comming 
+    setup(props){
+        props.posts.forEach(element => {
+            console.log(element.title);
+        });
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,55 +1,25 @@
 <template>
   <div class="home">
       <h1> Home </h1>
-      <input type="text" v-model="search">
-      <p> searched term -  {{ search }}</p>
-
-      <div v-for="name in matching" :key="name">
-        {{ name }}
-      </div>
-
-      <button @click="handleClick">stop watching</button>
-      
+      <PostList :posts="posts"/>
   </div>
 </template>
 
 <script>
+import PostList from '../components/PostList.vue'
 import { computed, reactive, ref, watch, watchEffect } from 'vue';
 
 
 export default {
   name: 'Home',
+  components: { PostList },
   setup () {
+    const posts = ref([
+      { title: 'Welcome to the blog', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat nihil nisi doloribus suscipit magni iusto porro harum, eveniet natus dicta vel modi accusamus reiciendis, unde vero debitis cupiditate ratione. Consectetur velit, eveniet labore vero ad ipsa sint quis dolorum. Blanditiis perferendis eos quisquam obcaecati commodi error, suscipit, a autem corrupti magni voluptatibus quo eligendi praesentium atque dolor veritatis totam! Iste, autem facilis? Sed veniam voluptate dolores similique, perspiciatis, itaque repellendus minus rerum praesentium molestias fugit quidem est neque ullam enim ducimus doloribus reiciendis a voluptatum. Quod facere molestiae, in quo itaque quasi vero, rerum sit commodi odit perspiciatis necessitatibus labore?', id: 1 },
+      { title: 'Top 5 css tips', body: 'lorem ipsum' , id: 2 },
+    ]);
 
-    let search = ref('');
-
-    // watch(search, ()=> console.log('watch function ran'))
-
-    // watchEffect(()=>{
-    //   console.log('watchEffect function is ran '+search.value);
-    // })
-
-
-    // How to stop watch or watchEffect is to invoke them by 
-    const Stopwatch = watch(search, ()=> console.log('watch function ran'))
-    const StopwatchEffect = watchEffect(()=>{ console.log('watchEffect function is ran '+search.value); });
-
-    function handleClick() {
-      Stopwatch();
-      StopwatchEffect();
-    }
-    // End stop watch and watchEffect
-
-    let names = ref(['Mouhsine', 'java', 'python', 'javascript', 'Math', 'physics'])
-
-    let matching = computed( () => {
-      return names.value.filter( name => {
-        return name.includes(search.value);
-
-      })
-    })
-
-    return {  search, names, matching, handleClick }
+    return { posts }
   }
 }
 </script>
